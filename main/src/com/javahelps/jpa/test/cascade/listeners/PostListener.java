653a5fb8074163ae75c1160e4e29277373b66620
Ivan Cherepica <ussr211114@gmail.com>
2020-05-20 00:46:58 +0300

    Добавлен пример с entity listener

--- main/src/com/javahelps/jpa/test/cascade/listeners/PostListener.java
+++ main/src/com/javahelps/jpa/test/cascade/listeners/PostListener.java
@@ -3,7 +3,6 @@ package com.javahelps.jpa.test.cascade.listeners;
 import com.javahelps.jpa.test.cascade.listeners.model.Post;
 import com.javahelps.jpa.test.cascade.listeners.model.PostComment;
 import com.javahelps.jpa.test.cascade.listeners.model.RelatedTheme;
-import com.javahelps.jpa.test.cascade.ondelete.DeleteCascadeProblem_1;
 import com.javahelps.jpa.test.util.PersistentHelper;
 
 import javax.persistence.EntityManager;
@@ -17,6 +16,10 @@ public class PostListener {
         EntityManager entityManager = PersistentHelper.getEntityManager(new Class[] {Post.class, PostComment.class, RelatedTheme.class});
 
         entityManager.getTransaction().begin();
+
+        System.out.println();
+        System.out.println("cascade removing by entity listener");
+        System.out.println();
 
         List<PostComment> postComments = entityManager
                 .createQuery("FROM " +PostComment.class.getSimpleName()+" pc WHERE pc.post.id = :postId", PostComment.class)
--- main/src/com/javahelps/jpa/test/cascade/listeners/model/Post.java
+++ main/src/com/javahelps/jpa/test/cascade/listeners/model/Post.java
@@ -3,46 +3,38 @@ package com.javahelps.jpa.test.cascade.listeners.model;
 import com.javahelps.jpa.test.cascade.listeners.PostListener;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
-    @Table(name = "post")
+@Table(name = "post")
 @EntityListeners(PostListener.class)
-    public class Post {
+public class Post {
+     @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
+     private Long id;
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
+     private String title;
 
-        private String title;
+     public Post() {
+     }
 
-        public Post() {
-        }
-
-        public Post(String title) {
+     public Post(String title) {
             this.title = title;
-        }
-
-        public Long getId() {
+     }
+     public Long getId() {
             return id;
-        }
+     }
 
-        public void setId(Long id) {
+     public void setId(Long id) {
             this.id = id;
         }
 
-        public String getTitle() {
+     public String getTitle() {
             return title;
         }
 
-        public void setTitle(String title) {
+     public void setTitle(String title) {
             this.title = title;
         }
 
-        @Override
-        public String toString() {
-            return "Post{" +
-                    "id=" + id +
-                    ", title='" + title + '\'' +
-                    '}';
-        }
-    }
+}
